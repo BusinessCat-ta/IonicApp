157aa73828cb3f4bbded04788cdf5b868492246e
BusinessCat-ta <stefano.cattarin@stud.tecnicosuperiorekennedy.it>
2020-07-31 16:25:13 +0200

    fix

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,20 +9,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  query: QueryModel;
+
   list;
 
-  constructor(private Api: ApiServiceService) {}
+  constructor(private Api: ApiServiceService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.queryBuild();
   }
 
-  queryBuild= () => {
-      this.query.column = "name, c_bpartner_id, ad_client_id, leadstatus, phone";
-      this.query.table = "ad_user";
-      this.query.where ="leadstatus != 'null'"
-      this.Api.getData(this.query).subscribe((data)=>{this.list = data});
+  queryBuild = () => {
+    let query: QueryModel;
+    query.column = "name, c_bpartner_id, ad_client_id, leadstatus, phone";
+    query.table = "ad_user";
+    query.where = "leadstatus != 'null'"
+    this.Api.getData(query).subscribe((data) => { this.list = data });
   }
 
 
